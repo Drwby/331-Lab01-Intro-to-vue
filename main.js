312ae31eb676440255b42ee9c42496a1ed10ac3b
Drwby <Kevin_b@cmu.ac.th>
2025-07-04 10:28:52 +0700

    8.5 commit

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const { createApp, ref } = Vue
+const { createApp, ref, computed } = Vue
 
 createApp({
     setup(){
         const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(false)
+        const brand =ref('SE 331')
+        //const image = ref('./assets/images/socks_green.jpg')
+        //const inStock = ref(false)
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
@@ -12,13 +13,18 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
-            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0}
         ])
+        const selectedVariant = ref(0)
         const cart = ref(0)
         function addToCart(){
             cart.value +=1
         }
+        const title = computed(() => {
+            return brand.value + ' ' + product.value
+        })
+
         function updateImage(variantImage){
             image.value = variantImage
         }
@@ -27,8 +33,28 @@ createApp({
         ])
         const productLink = ref('https://www.camt.cmu.ac.th/')
         const onSale = ref(true)
+        const saleMessage = computed(() => {
+    return onSale.value 
+        ? `${brand.value} ${product.value} is on sale` 
+        : `${brand.value} ${product.value} is not on sale`;
+});
+
+
+        function updateVariant(index) {
+        selectedVariant.value = index;
+        }
+
+        const image = computed(() => {
+        return variants.value[selectedVariant.value].image
+        })
+
+        const inStock = computed(() => {
+        return variants.value[selectedVariant.value].quantity
+        })
+
+
         return {
-            product,
+
             image,
             inStock,
             productLink,
@@ -39,7 +65,9 @@ createApp({
             sizes,
             cart,
             addToCart,
-            updateImage
+            updateImage,
+            title,
+            updateVariant
 
         }
   }
