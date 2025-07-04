c9d1a21e403754a7988470afb3ff8b0f1b60ffdf
Drwby <Kevin_b@cmu.ac.th>
2025-07-04 09:33:39 +0700

    Last Class

---
+++ main.js
@@ -0,0 +1,47 @@
+const { createApp, ref } = Vue
+
+createApp({
+    setup(){
+        const product = ref('Boots')
+        const image = ref('./assets/images/socks_green.jpg')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            {id: 2234, color: 'green', image: './assets/images/socks_green.jpg'},
+            {id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
+        ])
+        const cart = ref(0)
+        function addToCart(){
+            cart.value +=1
+        }
+        function updateImage(variantImage){
+            image.value = variantImage
+        }
+        const sizes = ref([
+            'S','M','L'
+        ])
+        const productLink = ref('https://www.camt.cmu.ac.th/')
+        const onSale = ref(true)
+        return {
+            product,
+            image,
+            inStock,
+            productLink,
+            inventory,
+            onSale,
+            details,
+            variants,
+            sizes,
+            cart,
+            addToCart,
+            updateImage
+
+        }
+  }
+}).mount('#app')
+
